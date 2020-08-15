d92c920af949741190c7449f024c0b04c9c6a79b
Rshivam069 <69722846+Rshivam069@users.noreply.github.com>
2020-08-15 23:16:40 +0530

    Update Assign_1.java

--- Day1/Assign_1.java
+++ Day1/Assign_1.java
@@ -2,7 +2,7 @@
 public class Assign_1 {
 
 	public static void main(String[] args) {
-		
+		// Input values
 		float item1=145;
 		float item2=299;
 		float item3=100;
@@ -10,6 +10,7 @@ public class Assign_1 {
 		float tax=15;
 		float tax_amt=(tax/100)*sum;
 		float final_amt=sum+tax_amt;
+		//Output
 		System.out.println("Tax Amount is Rs."+tax_amt);
 		System.out.println("Final Amount is Rs."+final_amt);
 	}
